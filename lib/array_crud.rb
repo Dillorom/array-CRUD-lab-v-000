d11801bc61b22b1052507f3d0456e913c5ebb322
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-28 17:34:09 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_elment_to_end_of_array(array, element) << ""
+  add_elment_to_end_of_array(array, element) << "5"
   
 end
 
